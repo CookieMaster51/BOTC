3dc69483d1d12e150bc62fdf6d3a0b9e67b8be3a
CookieMaster51 <jan.matis.2008@gmail.com>
2023-12-19 10:27:01 +0000

    ADDED TOO MUCHHHH

--- botc_helper.py
+++ botc_helper.py
@@ -1,6 +1,6 @@
 import discord
 class Player:
-    def __init__(self, role:str, alignment_good:bool, discord_id:int, discord_nick:str, has_ghost_voted:bool = False, is_dead:bool = False, reminders:list = []):
+    def __init__(self, role:str, alignment_good:bool, discord_id:int, discord_nick:str, has_ghost_voted:bool = False, is_dead:bool = False, reminders:list = [], bluffs:list = []):
         self.role = role
         self.discord_id = discord_id
         self.has_ghost_voted = has_ghost_voted
@@ -8,7 +8,16 @@ class Player:
         self.reminders = reminders
         self.alignment_good = alignment_good
         self.discord_nick = discord_nick
+        self.bluffs = bluffs
 
     def __repr__(self) -> str:
-        return f"name: {self.discord_nick}; role : {self.role}\n"
+        return f"name: {self.discord_nick}; role : {self.role}" if self.bluffs == [] else f"name: {self.discord_nick}; role : {self.role}; bluffs : {self.bluffs}"
+
+class Game:
+    demon = []
+    minions = []
+    outsiders = []
+    townsfolk = []
     
+    def __repr__(self) -> str:
+        return str(list(map(str, self.demon)) + list(map(str, self.minions)) + list(map(str, self.outsiders)) + list(map(str, self.townsfolk))) + "\n"
--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 # TODO: Add more logic, king, choirboy, riot, legion, huntsman
 #       Change the discord bot intents
 #       Make existing logic look better
-#       Add demon bluffs
 #       Add json exporting
 #       Add automatic role distribution
-#       Add automatic private vc generation
 #       Testing of weirder scripts
 
 import discord
@@ -13,6 +11,7 @@ import botc_helper
 import ast
 import random
 import copy
+import discord.utils
 
 intents = discord.Intents().all() # Might make this better than all of the intents
 bot = commands.Bot(command_prefix="!", intents = intents) 
@@ -22,7 +21,7 @@ async def on_ready(): # Mostly constants
     print(f"Logged in as {bot.user}")
     bot.script_og = []
     bot.script = []
-    bot.game = []
+    bot.game = botc_helper.Game()
     bot.count_to_numbers = {5:[3,0,1,1],
                             6:[3,1,1,1],
                             7:[5,0,1,1],
@@ -34,6 +33,7 @@ async def on_ready(): # Mostly constants
                             13:[9,0,3,1],
                             14:[9,1,3,1],
                             15:[9,2,3,1]}
+    bot.PRIVATE_VCS_CATEGORY_ID = 1186598911096406046
 
 @bot.command()
 async def set_script(ctx, *, arg):
@@ -46,8 +46,9 @@ async def new_game(ctx, number, player_role_id):
     counts = []
     delta_outsiders = 0
     bot.script = copy.deepcopy(bot.script_og)
-    bot.game = []
+    bot.game = botc_helper.Game()
     player_ids = [] 
+    is_demon = True
 
     for member in ctx.guild.members: # Goes through every member
         for role in member.roles: # Goes through their roles
@@ -58,10 +59,11 @@ async def new_game(ctx, number, player_role_id):
 
     demon_id = random.choice(player_ids)
     demon_role = random.choice(bot.script[3]["roles"])
-    bot.game.append(botc_helper.Player(demon_role, False, demon_id, ctx.guild.get_member(demon_id).display_name)) # Adds a demon to the game
-    player_ids.remove(bot.game[0].discord_id)
+    bot.game.demon.append(botc_helper.Player(demon_role, False, demon_id, ctx.guild.get_member(demon_id).display_name)) # Adds a demon to the game
+    player_ids.remove(demon_id)
 
-    if bot.game[0].role == "lil_monsta": # UNTESTED LOGIC, hopefully works
+    if bot.game.demon[0].role == "lil_monsta": # UNTESTED LOGIC, hopefully works
+        is_demon = False
         bot.game = []
         counts[2] += 1
         counts[3] = 0
@@ -72,7 +74,7 @@ async def new_game(ctx, number, player_role_id):
         curr_minion_id = random.choice(player_ids)
         minion_role = random.choice(bot.script[2]["roles"])
 
-        if demon_role == "fang_gu": # More edge-case logic, might want to make this neater later
+        if demon_role == "fang_gu": # More edge-case logic, might want to make this neater later, also need testing
             delta_outsiders += 1
         if minion_role == "baron":
             delta_outsiders += 2
@@ -83,7 +85,7 @@ async def new_game(ctx, number, player_role_id):
         elif minion_role == "godfather":
             delta_outsiders += random.choice([1, -1]) # Fuck you storyteller; it's random
 
-        bot.game.append(botc_helper.Player(minion_role, False, curr_minion_id, ctx.guild.get_member(curr_minion_id).display_name)) # Add the minion
+        bot.game.minions.append(botc_helper.Player(minion_role, False, curr_minion_id, ctx.guild.get_member(curr_minion_id).display_name)) # Add the minion
         player_ids.remove(curr_minion_id)
         bot.script[2]["roles"].remove(minion_role)
 
@@ -94,18 +96,55 @@ async def new_game(ctx, number, player_role_id):
     for i in range(counts[1]): # Adding outsiders, copy-pasted logic
         curr_outsider_id = random.choice(player_ids)
         curr_outsider_role = random.choice(bot.script[1]["roles"])
-        bot.game.append(botc_helper.Player(curr_outsider_role, True, curr_outsider_id, ctx.guild.get_member(curr_outsider_id).display_name))
+        bot.game.outsiders.append(botc_helper.Player(curr_outsider_role, True, curr_outsider_id, ctx.guild.get_member(curr_outsider_id).display_name))
         player_ids.remove(curr_outsider_id)
         bot.script[1]["roles"].remove(curr_outsider_role)
 
     for i in range(len(player_ids)): # Looping through rest of the player ids to assign them townsfolk 
         curr_townsfolk_id = random.choice(player_ids)
         curr_townsfolk_role = random.choice(bot.script[0]["roles"])
-        bot.game.append(botc_helper.Player(curr_townsfolk_role, True, curr_townsfolk_id, ctx.guild.get_member(curr_townsfolk_id).display_name))
+        bot.game.townsfolk.append(botc_helper.Player(curr_townsfolk_role, True, curr_townsfolk_id, ctx.guild.get_member(curr_townsfolk_id).display_name))
         player_ids.remove(curr_townsfolk_id)
         bot.script[0]["roles"].remove(curr_townsfolk_role)
 
+    if is_demon:
+        bluffs = []
+        for i in range(3): # loops through the 3 bluffs
+            bluff = random.choice(bot.script[0]["roles"]) # get a random townsfolk role
+            bot.script[0]["roles"].remove(bluff) 
+            bluffs.append(bluff)
+
+        for index, demon in enumerate(bot.game.demon):
+            bot.game.demon[index].bluffs = bluffs # Give it to all of the demons
+
     await ctx.reply(bot.game) # Sends a message in the channel to show the grimoir
+
+@bot.command()
+async def purge_vcs(ctx):
+    for channel in discord.utils.get(ctx.guild.categories, id = bot.PRIVATE_VCS_CATEGORY_ID).channels: # Goes through every channel in the privatevcs category
+        await channel.delete() # Gets rid of it
+
+@bot.event
+async def on_voice_state_update(member, before, after):
+    TWO_PLAYER_CREATE = 1186328181758763169
+    THREE_PLAYER_CREATE = 1186604174130024559
+    FOUR_PLAYER_CREATE = 1186604215343255573
+    
+
+    if after.channel.id == TWO_PLAYER_CREATE:
+        await after.channel.guild.create_voice_channel(f"2 player {member.display_name}", category = discord.utils.get(after.channel.guild.categories, id = bot.PRIVATE_VCS_CATEGORY_ID), user_limit = 2)
+
+    if after.channel.id == THREE_PLAYER_CREATE:
+        await after.channel.guild.create_voice_channel(f"3 player {member.display_name}", category = discord.utils.get(after.channel.guild.categories, id = bot.PRIVATE_VCS_CATEGORY_ID), user_limit = 3)
+
+    if after.channel.id == FOUR_PLAYER_CREATE:
+        await after.channel.guild.create_voice_channel(f"4 player {member.display_name}", category = discord.utils.get(after.channel.guild.categories, id = bot.PRIVATE_VCS_CATEGORY_ID), user_limit = 4)
+
+
+    if before.channel.category.id == bot.PRIVATE_VCS_CATEGORY_ID:
+        if len(before.channel.members) == 0: # Checks if there are 0 players in that vc
+            await before.channel.delete()
+    
 
 token = open("token.txt", "r") # You aint getting my token you sneaky boi
 bot.run(token.readline())
