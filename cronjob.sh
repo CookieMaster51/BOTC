4607ffe47cb9cc66bd9e76a37a7865426a2d2847
Jan Matis <76244176+CookieMaster51@users.noreply.github.com>
2024-01-06 18:42:02 +0000

    Update cronjob.sh

--- cronjob.sh
+++ cronjob.sh
@@ -2,11 +2,11 @@
 
 output=$(git pull)
 if [ "${output}" != "Already up to date." ]; then
-  pid=$(ps -ef | grep -v grep | grep botc-main.py | awk '{print $2}')
-  if [ -n "${pid}" ]; then
+  for pid in $(ps -ef | grep -v grep | grep botc-main.py | awk '{print $2}')
+  do 
     kill -9 ${pid}
-    sleep 5
-  fi
+  done
+  sleep 5
   python3 botc-main.py &
 else
  pid=$(ps -ef | grep -v grep | grep botc-main.py | awk '{print $2}')
